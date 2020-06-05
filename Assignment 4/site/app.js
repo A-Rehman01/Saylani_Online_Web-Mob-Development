e4efa6c5a5904e4cb5e4f20acde1910e73126b28
abdulrehmanwindow <arehmana377@gmail.com>
2020-06-05 03:41:33 -0700

    Rice and Millet Static Website

---
+++ Assignment 4/site/app.js
@@ -0,0 +1,19 @@
+// dropdown-toggle class not added for submenus by current WP Bootstrap Navwalker as of November 15, 2017.
+$('.dropdown-menu > .dropdown > a').addClass('dropdown-toggle');
+
+$('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
+  if (!$(this).next().hasClass('show')) {
+    $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+  }
+  var $subMenu = $(this).next(".dropdown-menu");
+  $subMenu.toggleClass('show');
+  $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+    $('.dropdown-menu > .dropdown .show').removeClass("show");
+  });
+  return false;
+});
+
+
+
+
+
